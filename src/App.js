d8471a7e75df39bfd4fddb5fba3fbd807a3c71ad
eatmoarrice <63140320+eatmoarrice@users.noreply.github.com>
2020-08-03 11:21:02 +0700

    update server

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import NavBarHey from "./components/NavBar";
-import PostIssue from "./components/PostIssue";
-import PageNotFound from "./components/PageNotFound";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import IssuePage from "./components/IssuePage";
-import HomePage from "./components/HomePage";
-import AllIssuesPage from "./components/AllIssuesPage";
-import Redirect404 from "./components/Redirect404";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faRobot } from "@fortawesome/free-solid-svg-icons";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavBarHey from './components/NavBar';
+import PostIssue from './components/PostIssue';
+import PageNotFound from './components/PageNotFound';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import IssuePage from './components/IssuePage';
+import HomePage from './components/HomePage';
+import AllIssuesPage from './components/AllIssuesPage';
+import Redirect404 from './components/Redirect404';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faRobot } from '@fortawesome/free-solid-svg-icons';
 library.add(fab, faRobot);
 
 const clientId = process.env.REACT_APP_CLIENT_ID;
@@ -20,8 +20,8 @@ const clientId = process.env.REACT_APP_CLIENT_ID;
 function App() {
 	const [token, setToken] = useState(null);
 	const getToken = () => {
-		const existingToken = localStorage.getItem("token");
-		const accessToken = window.location.search.split("=")[0] === "?access_token" ? window.location.search.split("=")[1].split("&")[0] : null;
+		const existingToken = localStorage.getItem('token');
+		const accessToken = window.location.search.split('=')[0] === '?access_token' ? window.location.search.split('=')[1].split('&')[0] : null;
 
 		if (!accessToken && !existingToken) {
 			window.location.replace(`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`);
@@ -30,7 +30,7 @@ function App() {
 		if (accessToken) {
 			console.log(`New accessToken: ${accessToken}`);
 
-			localStorage.setItem("token", accessToken);
+			localStorage.setItem('token', accessToken);
 
 			setToken(accessToken);
 		}
